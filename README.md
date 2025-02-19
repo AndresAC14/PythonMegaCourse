d30e5b465abdcc3476a0767b6feb704015dc01f6
AndresAC14 <andresamo0101@gmail.com>
2025-02-19 13:01:06 +0100

    docs: updating repo structure and adding .gitignore and Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Mega Course (Udemy)
+
+Git repository to store, files, scripts and exercises from the Python Mega Course.
+
+
+
+[Link to the course](https://www.udemy.com/course/former-python-mega-course-build-10-real-world-applications/)
--- test1.py
+++ test1.py
@@ -1,1 +1,31 @@
-print("Hello worlkd")
+'''
+print("Hello world")
+
+# Se pueden printeear varias cosas con print(v1, v2, v3, .., vn)
+
+x = 10
+y = "10"
+z = x + y
+
+print(z)
+
+
+# LIST count how many items are equal to 10.0 -> son mutables
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+hm = student_grades.count(10.0)
+
+print(hm)
+
+
+
+# DICTIONARY
+dict1 = {"Clave": "valor", "Huevo": 2}
+
+print(dict1.keys())
+'''
+
+# TUPLE -> son inmutables
+tupla = (1, 2, 3)
+
+tupla
